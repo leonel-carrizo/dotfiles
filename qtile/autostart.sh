c9d907756bf0417a38b537b85a14971e124b5ee8
l3o <l3o@archssd.localdomain>
2023-10-01 15:55:07 +0100

    add pulseAudio & natural scrolling

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-setxkbmap latam &
+setxkbmap gb &
 nitrogen --restore &
+systemctl --user enable pulseaudio
 picom &
+# change the touchpad behaviour
+xinput set-prop 12 "libinput Natural Scrolling Enabled" 1 
