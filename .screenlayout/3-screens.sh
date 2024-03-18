b58ba31bdf51ab782a409c5bb2eb2ddb77c28b15
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-03-18 18:00:00 +0000

    Add layout for 3 screens 3-screens.sh and set notification for keyboard layout
    I have modified the shortcut for change the keyboard layout and the script was imprubed, add a new screipt for manage extra monitor.

---
+++ .screenlayout/3-screens.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xrandr --output eDP-1 --primary --mode 1920x1080 --rate 59.93 --pos 1920x0 --rotate normal \
+	--output HDMI-1 --mode 1920x1080 --rate 129.88 --pos 3840x0 --rotate normal \
+	--output DP-1 --mode 1920x1080  --pos 0x0 --rotate normal \
+	--output DP-2 --off \
+	&& ~/.fehbg
