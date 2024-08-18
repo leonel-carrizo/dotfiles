75aee7a2f43b1477e3451d04e5c15a492a9dc0a9
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-08-18 16:21:18 +0100

    change list! settings to use the NVchad colors

--- nvim/options.lua
+++ nvim/options.lua
@@ -1,16 +1,13 @@
 require "nvchad.options"
--- add yours here!
 local o = vim.o
-local g = vim.g
 local opt = vim.opt
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt ='both' -- to enable cursorline!
 o.autoindent = true
 o.expandtab = false
 o.tabstop = 4
 o.shiftwidth = 4
 o.softtabstop=0
 ------------------------------ Sets hidden editing ----------------------------
-opt.listchars = { eol = "↓", space = "·", trail = "●", tab = "――", extends = ">", precedes = "<" }
-opt.highlight.SpecialKey = { ctermfg = "DarkGray" }
-opt.highlight.NonText = { ctermfg = "DarkGray" }
+opt.listchars = { eol = "↓", space = "·", trail = "●", tab = "―― ", extends = ">", precedes = "<" }
 o.wrap = false
+
