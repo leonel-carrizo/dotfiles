58d3e431fba85b8194f9d96d23eb17dcc2172c45
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-08-27 21:15:12 +0100

    add 42 header plugin

--- my_nvim/lua/plugins/init.lua
+++ my_nvim/lua/plugins/init.lua
@@ -1,20 +1,32 @@
 return {
 	"echasnovski/mini.icons",
-	{"nvim-lua/plenary.nvim", lazy = true },
+	{ "nvim-lua/plenary.nvim", lazy = true },
 	{ "MunifTanjim/nui.nvim", lazy = true },
 	{
-    "nvim-tree/nvim-web-devicons",
-    opts = function()
-      return { override = require "utils.devicons" }
-    end,
+		"nvim-tree/nvim-web-devicons",
+		opts = function()
+			return { override = require("utils.devicons") }
+		end,
 	},
 	-- basic buffer manager taken form Nvchad
-	{ dir = "~/.config/nvim/lua/local/term",	lazy = true },
+	{ dir = "~/.config/nvim/lua/local/term", lazy = true },
 	{
 		dir = "~/.config/nvim/lua/local/tabufline",
 		-- enabled = false,
+		config = function()
+			require("local.tabufline.lazyload")
+		end,
+	},
+	-- header 42London
+	{
+		dir = "$HOME/.vim/plugin/stdheader.vim",
+		lazy = true,
+		cmd = "Stdheader",
+		keys =
+			{"<F1>", "<cdm>Stdheader<CR>", mode = 'n', remap = true, silent = true, desc = "42 Header for C Files."},
+		ft = {"c", "Makefile"},
 		config = function ()
-				require("local.tabufline.lazyload")
-			end
+			vim.cmd("source $HOME/.vim/plugin/stdheader.vim")
+		end
 	},
 }
