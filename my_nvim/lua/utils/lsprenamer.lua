3ff91b601608e1250e042c7072fdef46a011042e
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-08-30 11:07:25 +0100

    add function get_clients

--- my_nvim/lua/utils/lsprenamer.lua
+++ my_nvim/lua/utils/lsprenamer.lua
@@ -41,6 +41,24 @@ function M.rename()
   end, { buffer = 0 })
 end
 
+--@param opts? lsp.Client.filter
+function M.get_clients(opts)
+  local ret = {} ---@type vim.lsp.Client[]
+  if vim.lsp.get_clients then
+    ret = vim.lsp.get_clients(opts)
+  else
+    ---@diagnostic disable-next-line: deprecated
+    ret = vim.lsp.get_active_clients(opts)
+    if opts and opts.method then
+      ---@param client vim.lsp.Client
+      ret = vim.tbl_filter(function(client)
+        return client.supports_method(opts.method, { bufnr = opts.bufnr })
+      end, ret)
+    end
+  end
+  return opts and opts.filter and vim.tbl_filter(opts.filter, ret) or ret
+end
+
 ---@param from string
 ---@param to string
 ---@param rename? fun()
