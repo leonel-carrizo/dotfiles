71b54b0fac5c94329363b4ff662cf3bcef4edb24
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-08-30 11:01:16 +0100

    fomat  file

--- my_nvim/lua/plugins/notifications.lua
+++ my_nvim/lua/plugins/notifications.lua
@@ -1,34 +1,34 @@
 return {
-	{
-	 "folke/noice.nvim",
-     event = "VeryLazy",
-     opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
             },
-          },
-          view = "mini",
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        any = {
+                            { find = "%d+L, %d+B" },
+                            { find = "; after #%d+" },
+                            { find = "; before #%d+" },
+                        },
+                    },
+                    view = "mini",
+                },
+            },
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+            },
         },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-      },
-    },
     -- stylua: ignore
     keys = {
       { "<leader>sn", "", desc = "+noice"},
@@ -41,58 +41,58 @@ return {
       { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
       { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
     },
-	--	https://github.com/folke/noice.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
-    config = function(_, opts)
-      -- HACK: noice shows messages from before it was enabled,
-      -- but this is not ideal when Lazy is installing plugins,
-      -- so clear the messages in this case.
-      if vim.o.filetype == "lazy" then
-        vim.cmd([[messages clear]])
-      end
-      require("noice").setup(opts)
-    end,
-	dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    lazy = true,
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-    }
-	},
-	-- Better `vim.notify()`
-  {
-    "rcarriga/nvim-notify",
-    keys = {
-      {
-        "<leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
+        --	https://github.com/folke/noice.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
+        config = function(_, opts)
+            -- HACK: noice shows messages from before it was enabled,
+            -- but this is not ideal when Lazy is installing plugins,
+            -- so clear the messages in this case.
+            if vim.o.filetype == "lazy" then
+                vim.cmd [[messages clear]]
+            end
+            require("noice").setup(opts)
         end,
-        desc = "Dismiss All Notifications",
-      },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            lazy = true,
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
     },
-    opts = {
-      stages = "static",
-      timeout = 5000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-      on_open = function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end,
+    -- Better `vim.notify()`
+    {
+        "rcarriga/nvim-notify",
+        keys = {
+            {
+                "<leader>un",
+                function()
+                    require("notify").dismiss { silent = true, pending = true }
+                end,
+                desc = "Dismiss All Notifications",
+            },
+        },
+        opts = {
+            stages = "static",
+            timeout = 5000,
+            max_height = function()
+                return math.floor(vim.o.lines * 0.75)
+            end,
+            max_width = function()
+                return math.floor(vim.o.columns * 0.75)
+            end,
+            on_open = function(win)
+                vim.api.nvim_win_set_config(win, { zindex = 100 })
+            end,
+        },
+        init = function()
+            -- when noice is not enabled, install notify
+            if not pcall(require, "noice") then
+                vim.defer_fn(function()
+                    vim.notify = require "notify"
+                end, 0)
+            end
+        end,
     },
-    init = function()
-      -- when noice is not enabled, install notify
-	  if not pcall(require, "noice") then
-        vim.defer_fn(function()
-          vim.notify = require("notify")
-        end, 0)
-      end
-    end,
-  },
 }
