3134b4c4d5eacbb62ab16ddb09f13bdf6728f231
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-08-31 12:13:23 +0100

    change keymap settings

--- my_nvim/lua/plugins/42.lua
+++ my_nvim/lua/plugins/42.lua
@@ -5,19 +5,39 @@ return
 		dir = "$HOME/.vim/plugin/stdheader.vim",
 		lazy = true,
 		cmd = "Stdheader",
-		keys =
-		{ "<F1>", "<cdm>Stdheader<CR>", mode = 'n', remap = true, silent = true, desc = "42 Header for C Files." },
+		keys = {
+			{ "<F1>", "<cdm>Stdheader<CR>", desc = "42 Header for C Files." },
+		},
 		ft = { "c", "Makefile" },
 		config = function()
 			vim.cmd("source $HOME/.vim/plugin/stdheader.vim")
 		end
 	},
-	-- formater
-	{
-
-	},
 	-- linter
 	{
+		"vinicius507/norme.nvim",
+		cmd = "NormeToggle",
+		keys = {
+			{ "<leader>cn", "<cmd>NormeToggle<CR>", mode = {"n", "v"}, desc = "Toggle Norminette linter on C/CPP files."},
+		},
+		ft = {"c", "cpp"},
+		dependencies = {
+				"jose-elias-alvarez/null-ls.nvim",
+				"nvim-lua/plenary.nvim",
+		},
+		config = function ()
+			require("null-ls").setup()
+			require("norme").setup()
+		end
+	},
+	-- formater
+	{
+			"cacharle/c_formatter_42.vim",
+			cmd = "CFormatter42",
+			keys = {
+				{ "<F2>", "<cmd>CFormatter42<CR>", mode = {"n", "v"}, desc = "Format C files for Norminette." },
+			},
+			ft = {"c", "cpp"},
 
 	},
 }
