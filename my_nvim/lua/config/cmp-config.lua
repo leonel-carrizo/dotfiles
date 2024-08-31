4b8449d4738bfc0e5890ebcd9cbb77c16da2e1c9
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-08-31 12:20:25 +0100

    change keymap for closing the cmp pupup

--- my_nvim/lua/config/cmp-config.lua
+++ my_nvim/lua/config/cmp-config.lua
@@ -59,7 +59,7 @@ local options = {
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-u>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
+		["<C-.>"] = cmp.mapping.close(),
 
 		["<CR>"] = cmp.mapping.confirm {
 			behavior = cmp.ConfirmBehavior.Insert,
