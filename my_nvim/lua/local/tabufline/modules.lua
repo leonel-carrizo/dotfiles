e5fe488ca7ce6d25958f82dea6c3a77c5dcd0e9f
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-08-31 18:59:35 +0100

    adds function to open neo-tree with the toggle button

--- my_nvim/lua/local/tabufline/modules.lua
+++ my_nvim/lua/local/tabufline/modules.lua
@@ -27,7 +27,7 @@ vim.cmd [[
 vim.cmd "function! TbNewTab(a,b,c,d) \n tabnew \n endfunction"
 vim.cmd "function! TbGotoTab(tabnr,b,c,d) \n execute a:tabnr ..'tabnext' \n endfunction"
 vim.cmd "function! TbCloseAllBufs(a,b,c,d) \n lua require('local.tabufline').closeAllBufs() \n endfunction"
--- vim.cmd "function! TbToggle_theme(a,b,c,d) \n lua require('base46').toggle_theme() \n endfunction"
+vim.cmd "function! TbToggle_theme(a,b,c,d) \n lua require('neo-tree.command').execute({toggle = true}) \n endfunction"
 vim.cmd "function! TbToggleTabs(a,b,c,d) \n let g:TbTabsToggled = !g:TbTabsToggled | redrawtabline \n endfunction"
 
 -------------------------------------------------------- functions ------------------------------------------------------------
