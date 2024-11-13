0ed90853cc70069b108f25206381f7807c99e11c
l3o <102891741+leonel-carrizo@users.noreply.github.com>
2024-11-13 11:54:03 +0000

    change settings for gitsign

--- my_nvim/lua/plugins/git.lua
+++ my_nvim/lua/plugins/git.lua
@@ -2,32 +2,69 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
-		opts = function()
-			local options = {
+		config = function()
+			require("gitsigns").setup {
 				signs = {
+					add = { text = "▎" },
+					change = { text = "┃" },
 					delete = { text = "󰍵" },
+					topdelete = { text = "" },
 					changedelete = { text = "󱕖" },
+					untracked = { text = "┆" },
 				},
-
-				on_attach = function(bufnr)
+				signs_staged = {
+					add = { text = "▎" },
+					change = { text = "┃" },
+					delete = { text = "󰍵" },
+					topdelete = { text = "" },
+					changedelete = { text = "┆" },
+				},
+				on_attach = function(buffer)
 					local gs = package.loaded.gitsigns
 
-					local function opts(desc)
-						return { buffer = bufnr, desc = desc }
+					local function map(mode, l, r, desc)
+						vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
 					end
 
-					local map = vim.keymap.set
-
-					map("n", "<leader>rh", gs.reset_hunk, opts "Reset Hunk")
-					map("n", "<leader>ph", gs.preview_hunk, opts "Preview Hunk")
-					map("n", "<leader>gb", gs.blame_line, opts "Blame Line")
+					map("n", "]h", function()
+						if vim.wo.diff then
+							vim.cmd.normal { "]c", bang = true }
+						else
+							gs.nav_hunk "next"
+						end
+					end, "Next Hunk")
+					map("n", "[h", function()
+						if vim.wo.diff then
+							vim.cmd.normal { "[c", bang = true }
+						else
+							gs.nav_hunk "prev"
+						end
+					end, "Prev Hunk")
+					map("n", "]H", function()
+						gs.nav_hunk "last"
+					end, "Last Hunk")
+					map("n", "[H", function()
+						gs.nav_hunk "first"
+					end, "First Hunk")
+					map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+					map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+					map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+					map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+					map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+					map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
+					map("n", "<leader>ghb", function()
+						gs.blame_line { full = true }
+					end, "Blame Line")
+					map("n", "<leader>ghB", function()
+						gs.blame()
+					end, "Blame Buffer")
+					map("n", "<leader>ghd", gs.diffthis, "Diff This")
+					map("n", "<leader>ghD", function()
+						gs.diffthis "~"
+					end, "Diff This ~")
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
 				end,
 			}
-
-			return options
-		end,
-		config = function()
-			require("gitsigns").setup()
 		end,
 	},
 	{
